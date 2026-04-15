bb967491f4bb0970b4c8984010cd73aaf4e4bf37
Satyam19113 <Satyamkr1354@gmail.com>
2026-04-15 22:01:13 +0530

    frontend ready for deployment

--- frontend/src/api/newsApi.js
+++ frontend/src/api/newsApi.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://127.0.0.1:8000/v1'
+// const BASE_URL = 'http://127.0.0.1:8000/v1'
+const BASE_URL = import.meta.env.VITE_API_URL
+
 
 const api = axios.create({
   baseURL: BASE_URL,
